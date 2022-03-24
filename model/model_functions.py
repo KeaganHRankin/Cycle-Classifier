b7e8a29b3bb231db6d10ca205491a79f7996f523
Keagan Rankin <keagan.rankin@gmail.com>
2022-03-24 00:04:27 -0400

    Baseline model 3/24/2022

--- model/model_functions.py
+++ model/model_functions.py
@@ -8,12 +8,14 @@ and comment every new function or class that is put in it.
 # Import 3rd party libraries:
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 # SCIKIT Learn IMPORTS:
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
@@ -27,6 +29,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import roc_curve
 from sklearn.metrics import precision_recall_curve
 
@@ -40,10 +43,69 @@ from scipy.stats import uniform
 
 
 # Feature Engineering Functions
+def ysign_droprows(data, keep):
+    """
+    Function drops rows that are not passed in 'keep' list.
+    """
+    
+    #Drop
+    data_drop = data[keep]
+    
+    return data_drop
 
+
+def ysign_scale(data, scale_feats):
+    """
+    Scales the passed columns of the ysign data.
+    """
+    
+    # Create scaler
+    scaler = StandardScaler()
+    scaler.fit(data[scale_feats])
+    
+    scaled = data[scale_feats].copy()
+    
+    # Convert numeric features to standard units
+    scaled = scaler.transform(scaled)
+    
+    data[scale_feats] = scaled
+    
+    return data
 
 # Feature Selection Functions
-
+def feature_selector(model, splits, X, y, i):
+    """ 
+    Selects features for the given model based on Kfold validation.
+    inputs are splits, model + xy, and i-1# of best features.
+    """
+    
+    # Initial setup
+    f1s = []
+    five_fold = StratifiedKFold(n_splits=splits)  #use stratified kfold due to calss imbalance.
+    #could also use normal k-fold and remove y from five_fold.split()
+    #five_fold = KFold(n_splits=splits)
+    
+    for train_index, val_index in five_fold.split(X, y):
+        
+        # Select i+1 best features
+        select = SelectKBest(chi2, k=i).fit(X.iloc[train_index, :],
+                                            y.iloc[train_index])
+        
+        # reduced feature set
+        X_new = X.loc[:,select.get_support()]
+        print('Selected features: {}'.format(X_new.columns.tolist()))
+        
+        # Fit model
+        model.fit(X_new.iloc[train_index, :],
+                  y.iloc[train_index])
+        
+        # Show prelim F1 score
+        f1s.append(f1_score(y.iloc[val_index],
+                            model.predict(X_new.iloc[val_index, :])
+                           )
+                  )
+        
+    print('Mean fold F1 Score: {}'.format(np.mean(f1s)))
 
 # Hyper Parameter Tuning Functions
 
