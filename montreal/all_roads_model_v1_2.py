9060fed446da3263e4616e7048da13271288bed8
Keagan Rankin <keagan.rankin@gmail.com>
2022-05-24 13:37:39 -0400

    Application of Model to the bike Network in Montreal - 2020-05-24

---
+++ montreal/all_roads_model_v1_2.py
@@ -0,0 +1,217 @@
+"""
+ALL ROADS MODEL -> USES the typology of the road as defined by the City or Provincial/Federal Government to make predicitons.
+
+This .py file deploys the proof-of-concept model for classifying the bike accessibility of roads.
+The model should be trained with appropriate data format (see example datasets in this repo).
+It can then output the predicited stress of a road for any city.
+1 = LOW STRESS (LTS 1 & 2)
+0 = HIGH STRESS (LTS 3 & 4)
+"""
+
+# Import required functions. See model_functions for nuts & bolts of training and results.
+from model_functions_v1_2 import *
+import pickle
+
+
+
+# Training the model
+def train(train_data, model_name, save_model=False, metrics=False):
+    """
+    trains the model given data and what model to use.
+    
+    train_data = training data in the format of centrelinebike_train_spatial.csv.
+    model_name = 'forest' or 'log' to train the model on either of these types of models.
+    save_model = True/False whether to pickle the model.
+    metrics = True/False whether to return training metrics.
+    """
+    
+    print("[INFO] starting training.")
+    
+    # Perform feature engineering
+    # Map the bikelanes in the correct way
+    train_data = map_centreline_features(train_data)
+    
+    # Split features/target
+    y_train_lts = train_data['LTS']
+    y_train_access = train_data['high access']
+    X_train = train_data.drop(['LTS','high access'], axis=1)
+    
+    # Engineer features usign model functions.
+    keep_rows = ['FEATURE_CODE_DESC','geometry', 'AREA_ID', 'bikelane']
+    X_train = droprows(X_train, keep_rows)
+    X_train = add_regions(X_train, 2, 3)
+    X_train = dummy(X_train, dummy_feats=['FEATURE_CODE_DESC','x_region','y_region'])
+    
+    print("[INFO] finished feature engineering.")
+    
+    # Define Features
+    features = ['_Arterial', '_Collector', '_Local', '_Trail',
+                #'x_region_2', 'x_region_3', 'y_region_2', 'y_region_3', 
+                'bikelane',]
+    
+    
+    # train based on the given model
+    if model_name == 'log':
+        model_log = LogisticRegression(C=0.9056753857212789, class_weight='balanced')
+        model = model_log.fit(X_train[features], y_train_access)
+        
+        print("[INFO] finished training logreg model.")
+
+    elif model_name == 'forest':
+        model_forest = RandomForestClassifier(criterion='entropy', max_features=0.5384874461913766,
+                                              max_samples=0.5862816454781242, class_weight='balanced')
+        model = model_forest.fit(X_train[features], y_train_access) 
+        
+        print("[INFO] finished training random forest model.")
+        
+    else:
+        print('[ERROR] not a valid model name: input "log" or "forest".')
+    
+    
+    # Save the model if this option was specified
+    if save_model:
+        file_name = 'model_1.pkl'
+        
+        with open(file_name, 'wb') as f:
+            pickle.dump(model, f)
+        
+        print(f'[INFO] saved model as {file_name}.')
+        
+        
+    # Return training cross validation metrics
+    if metrics:
+        print('[INFO] returning training metrics using spatial cv...')
+
+        # spatial_cv
+        spatial_cv(model, grouper=X_train['AREA_ID'], splits=141, X=X_train[features], y=y_train_access)
+        
+    else:
+        print('[INFO] skipping metrics.')
+    
+    print('[INFO] complete.')
+
+    return model
+
+
+
+# Predicting results using the model.
+def predict_test(data, model='', model_fname='model_1.pkl'):
+    """
+    Takes in a trained model (either just trained or from saved) and returns predictions + probabilities.
+    
+    data = test data in the format of centrelinebike_train_spatial.csv.
+    model = trained model to use. Can be left empty.
+    model_fname = model used if model name is not defined.
+    """
+    
+    # Perform feature engineering to get in same format as trained data.
+    # Map the bikelanes in the correct way
+    data = map_centreline_features(data)
+    
+    # Split features/target
+    #y_test_lts = data['LTS']
+    y_test_access = data['high access']
+    X_test = data.drop(['LTS','high access'], axis=1)
+    
+    # Engineer features usign model functions.
+    keep_rows = ['FEATURE_CODE_DESC','geometry', 'bikelane']
+    X_test = droprows(X_test, keep_rows)
+    X_test = add_regions(X_test, 2, 3)
+    X_test = dummy(X_test, dummy_feats=['FEATURE_CODE_DESC','x_region','y_region'])
+    
+    # Rename columns, removeing the 'FEATURE_CODE_DESC' from the title
+    
+    
+    print("[INFO] finished feature engineering")
+    
+    # Define Features - DONT USE TRAILS FOR NOW
+    features = ['_Arterial', '_Collector', '_Local', '_Trail',
+                #'x_region_2', 'x_region_3', 'y_region_2', 'y_region_3', 
+                'bikelane',]
+    
+    # Load Model
+    if not model:
+        
+        print(f"[INFO] opening model from file {model_fname} ")
+        
+        with open(model_fname, 'rb') as f:
+            model = pickle.load(f)
+
+    # Predict
+    y_pred = model.predict(X_test[features])
+    
+    try:
+        y_prob = model.predict_proba_(X_test[features])
+        
+    except AttributeError:
+        print('[ERROR] modeltype randomforest has no "predict_proba_". Not returning')
+        y_prob = 0
+    
+    # Report Label
+    print('[INFO] complete.')
+
+    return y_pred, y_prob
+
+
+
+
+# Predicting results using the model.
+def predict_general(data, model='', model_fname='model_1.pkl'):
+    """
+    Takes in a trained model (either just trained or from saved) and returns predictions + probabilities.
+    
+    data = test data in the format of centrelinebike_train_spatial.csv.
+    model = trained model to use. Can be left empty.
+    model_fname = model used if model name is not defined.
+    """
+    
+    # Perform feature engineering to get in same format as trained data.
+    # Map the bikelanes in the correct way
+    data = map_centreline_features(data)
+    
+    # Split features/target
+    X_test = data
+    
+    # Engineer features usign model functions.
+    keep_rows = ['FEATURE_CODE_DESC','geometry', 'bikelane']
+    X_test = droprows(X_test, keep_rows)
+    X_test = add_regions(X_test, 2, 3)
+    X_test = dummy(X_test, dummy_feats=['FEATURE_CODE_DESC','x_region','y_region'])
+    
+    # Rename columns, removeing the 'FEATURE_CODE_DESC' from the title
+    
+    
+    print("[INFO] finished feature engineering")
+    
+    # Define Features - DONT USE TRAILS FOR NOW
+    features = ['_Arterial', '_Collector', '_Local', '_Trail',
+                #'x_region_2', 'x_region_3', 'y_region_2', 'y_region_3', 
+                'bikelane',]
+    
+    #HARDCODING, MAKE A TRY EXCEPT:
+    # If any of the features don't exist, we want to add that feature as a zero column
+    X_test['_Trail'] = 0
+    
+    
+    # Load Model
+    if not model:
+        
+        print(f"[INFO] opening model from file {model_fname} ")
+        
+        with open(model_fname, 'rb') as f:
+            model = pickle.load(f)
+
+    # Predict
+    y_pred = model.predict(X_test[features])
+    
+    try:
+        y_prob = model.predict_proba_(X_test[features])
+        
+    except AttributeError:
+        print('[ERROR] modeltype randomforest has no "predict_proba_". Not returning')
+        y_prob = 0
+    
+    # Report Label
+    print('[INFO] complete.')
+
+    return y_pred, y_prob
---
+++ montreal/model_functions_v1_2.py
@@ -0,0 +1,419 @@
+"""
+This python file contains all of the functions and libraries required for the model to run.
+Files are imported into the _model.ipynb file for ease of use. Keep this file organized
+and comment every new function or class that is put in it.
+"""
+
+### THIRD PARTY LIBRARIES
+# Import 3rd party libraries:
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from shapely import wkt
+
+# SCIKIT Learn IMPORTS:
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import KFold, GroupKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_recall_curve
+
+from sklearn.utils.fixes import loguniform
+from scipy.stats import uniform
+
+
+### OUR FUNCTIONS
+
+# Data Cleaning Functions
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+# feature_engineering_functions
+
+
+def droprows(data, keep):
+    """
+    Function drops rows that are not passed in 'keep' list.
+    """
+    
+    #Drop
+    data_drop = data[keep]
+    
+    return data_drop
+
+
+def scale(data, scale_feats):
+    """ 
+    Just scales feats
+    """
+    # Create scaler
+    scaler = StandardScaler()
+    scaler.fit(data[scale_feats])
+    
+    scaled = data[scale_feats].copy()
+    
+    # Convert numeric features to standard units
+    scaled = scaler.transform(scaled)
+    data[scale_feats] = scaled
+    
+    return data
+
+
+def scale_minmax(data, scale_feats):
+    """ 
+    Just scales feats
+    """
+    # Create scaler
+    scaler = MinMaxScaler()
+    scaler.fit(data[scale_feats])
+    
+    scaled = data[scale_feats].copy()
+    
+    # Convert numeric features to standard units
+    scaled = scaler.transform(scaled)
+    data[scale_feats] = scaled
+    
+    return data
+
+
+def dummy(data, dummy_feats):
+    """
+    Just dummies features
+    """
+    for s in dummy_feats:
+        categorical = pd.get_dummies(data[s], prefix='',) #drop_first=True)
+        data = pd.concat((data, categorical), axis=1)
+        
+    data = data.drop(dummy_feats, axis=1)
+    
+    return data    
+
+
+def scale_and_dummy(data, scale_feats, dummy_feats):
+    """
+    Scales the passed columns of data.
+    Dummy encode categorical features passed.
+    """
+    
+    # Create scaler
+    scaler = StandardScaler()
+    scaler.fit(data[scale_feats])
+    
+    scaled = data[scale_feats].copy()
+    
+    # Convert numeric features to standard units
+    scaled = scaler.transform(scaled)
+    data[scale_feats] = scaled
+    
+    # Convert categorical features using dummy encoding. 
+    #Drop the encoded features from the frame.
+    for s in dummy_feats:
+        categorical = pd.get_dummies(data[s], prefix=s, drop_first=True)
+        data = pd.concat((data, categorical), axis=1)
+        
+    data = data.drop(dummy_feats, axis=1)
+    
+    return data
+
+
+def add_regions(data, components, bins):
+    """
+    function extract PC of Toronto's geometry and bins it into a # of regions.
+    
+    takes: in data, # of components, and # of bins.
+    returns: the training data with x_regions and y_regions labels for dummy encoding.
+    string
+    """
+    # Extract the centroids of the LINESTRING geometry.
+    # Also return the x y components of these centroids
+    data_g = data.copy()
+    data_g['geometry'] = data_g['geometry'].apply(wkt.loads)
+        
+    train_data_gpd = gpd.GeoDataFrame(data_g, crs="EPSG:26917")
+    train_data_gpd.head()
+    
+    centroids = train_data_gpd['geometry'].centroid
+    centroids_df = pd.DataFrame(data={'x':centroids.x, 'y':centroids.y})
+    
+    # Scale features.
+    scaler = StandardScaler().fit(centroids_df)
+    X_scaled = scaler.transform(centroids_df)
+    
+    # Create/fit PCA function.
+    pca = PCA(n_components=components)
+    X_transformed = pca.fit_transform(X_scaled)
+    X_transformed_df = pd.DataFrame(X_transformed)
+    
+    # Print results.
+    for i in range(components):
+        print('Principal component', i)
+        print('explains', (pca.explained_variance_ratio_[i] * 100), '% of the variance in "lon" and "lat".')
+    
+    # Return inversed values
+    X_new = pca.inverse_transform(X_transformed)
+    X_new = scaler.inverse_transform(X_new)
+    X_new_df = pd.DataFrame(X_new)
+    
+    # Bin geographic regions based on components
+    out = pd.qcut(X_transformed_df[0], bins, labels=np.arange(1,bins+1,1))
+    out2 = pd.qcut(X_transformed_df[1], bins, labels=np.arange(1,bins+1,1))   
+    
+    #Append the bin labels to the geo data.
+    data_g['x_region'] = out
+    data_g['y_region'] = out2
+    
+    #return X_transformed_df, centroids_df
+    return data_g
+
+
+def map_centreline_features(data):
+    """
+    Maps the centreline feature codes to their more like features.
+    """
+    data_m = data.copy()
+    data_m['FEATURE_CODE_DESC'] = data_m['FEATURE_CODE_DESC'].replace({'Laneway':'Local',
+                                                                       'Minor Arterial':'Arterial',
+                                                                       'Major Arterial':'Arterial',
+                                                                       'Major Arterial Ramp':'Arterial',
+                                                                       'Walkway':'Trail',
+                                                                       'Collector Ramp':'Collector',
+                                                                       'Minor Arterial Ramp':'Arterial',
+                                                                       'Private':'Other',
+                                                                        })
+    
+    
+    return data_m
+
+#--------------------------------------------------------------------------------------------------------------------------------
+# Feature Selection Functions
+
+
+def spatial_cv(model, grouper, splits, X, y):
+    """
+    Function performs spatial Cross Val based on the past grouping labels.
+    
+    model = sklearn model to be fit.
+    grouper = labels in the data's index order of spatial k-folds (ex. neighbourhood, clusters).
+    data = data 
+    """
+    #Init the group Kfold with n splits.
+    group_kfold = GroupKFold(n_splits=splits)
+    
+    i = 1
+    
+    f1s = []
+    accs = []
+    mse = []
+    #Loop Through the train and val index, perform cv
+    for train_index, val_index in group_kfold.split(X, y, grouper):
+        
+        # Fit model
+        model.fit(X.iloc[train_index, :],
+                  y.iloc[train_index])
+        
+        # Append scores for that loop
+        f1s.append(f1_score(y.iloc[val_index],
+                    model.predict(X.iloc[val_index, :]),
+                    average='weighted'))
+        
+        accs.append(accuracy_score(y.iloc[val_index],
+                    model.predict(X.iloc[val_index, :]),
+                  ))
+        
+        mse.append(mean_squared_error(y.iloc[val_index],
+                    model.predict(X.iloc[val_index, :]),
+                  ))
+        
+    #Print the results
+    print('Mean fold weighted F1 Score: {}'.format(np.mean(f1s)))
+    print('Mean fold accuracy Score: {}'.format(np.mean(accs)))
+    print('Mean fold MSE: {}'.format(np.mean(mse)))
+
+
+def feature_selector(model, splits, X, y, i):
+    """ 
+    Selects features for the given model based on Kfold validation.
+    inputs are splits, model + xy, and i# of best features.
+    """
+    
+    # Initial setup
+    f1s = []
+    five_fold = StratifiedKFold(n_splits=splits)  #use stratified kfold due to calss imbalance.
+    #could also use normal k-fold and remove y from five_fold.split()
+    #five_fold = KFold(n_splits=splits)
+    
+    for train_index, val_index in five_fold.split(X, y):
+        
+        # Select i+1 best features
+        select = SelectKBest(chi2, k=i).fit(X.iloc[train_index, :],
+                                            y.iloc[train_index])
+        
+        # reduced feature set
+        X_new = X.loc[:,select.get_support()]
+        print('Selected features: {}'.format(X_new.columns.tolist()))
+        
+        # Fit model
+        model.fit(X_new.iloc[train_index, :],
+                  y.iloc[train_index])
+        
+        # Show prelim F1 score
+        f1s.append(f1_score(y.iloc[val_index],
+                            model.predict(X_new.iloc[val_index, :]),
+                            average='weighted',
+                           )
+                  )
+        
+    print('Mean fold F1 Score: {}'.format(np.mean(f1s)))
+
+    
+#-------------------------------------------------------------------------------------------------------------------------------
+# Hyper Parameter Tuning Functions
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+# Evaluation Functions
+
+
+def f1_threshold(y_true, y_pred_proba, threshold):
+    
+    """
+    Returns the F-beta score.
+
+    Parameters:
+        y_true (1D numpy array): 1D array of true binary labels 
+                                 np.array([0, 1, 0, 0, ..]).
+        y_pred_proba (1D numpy array): 1D array of prediction probabilities 
+                                       for the positive class
+                                       (model.predict_proba(X)[:, 1])
+                                       np.array([0.12, 0.56, 0.23, 0.89, ..]).
+        threshold (float): The probability threshold, which is a number 
+                           between 0 and 1.
+
+    Returns:
+        (float): The F1 score given a threshold.
+    """
+    
+    # Calculate the binary predictions
+    y_pred = (y_pred_proba >= threshold).astype(int)
+    
+    return f1_score(y_true, y_pred, average='weighted')
+
+
+def plot_f1_threshold(X, y, model):
+    
+    """
+    Returns the F-beta score.
+
+    Parameters:
+        y_true (1D numpy array): 1D array of true binary labels 
+                                 np.array([0, 1, 0, 0, ..]).
+        y_pred_proba (1D numpy array): 1D array of prediction probabilities 
+                                       for the positive class
+                                       (model.predict_proba(X)[:, 1])
+                                       np.array([0.12, 0.56, 0.23, 0.89, ..]).
+        threshold (float): The probability threshold, which is a number 
+                           between 0 and 1.
+    """
+
+    # Create threshold array
+    thresholds = np.arange(0, 1, 0.01)
+    
+    # Compute F1 scores for each threshold
+    f1_scores = np.array([f1_threshold(y.values.flatten(), model.predict_proba(X)[:, 1], threshold) 
+                          for threshold in np.arange(0, 1, 0.01)])
+    
+    # Get finite values
+    thresholds = thresholds[np.isfinite(f1_scores)]
+    f1_scores = f1_scores[np.isfinite(f1_scores)]
+    
+    # Optimal values
+    idx = np.argmax(f1_scores)
+    f1_score = f1_scores[idx]
+    threshold = thresholds[idx]
+
+    # Plot
+    fig = plt.figure(figsize=(12, 4))
+    fig.subplots_adjust(wspace=0.2, hspace=0)
+    ax1 = plt.subplot2grid((1, 2), (0, 0))
+    ax2 = plt.subplot2grid((1, 2), (0, 1))
+    ax1.set_title('Optimal F1 score is {:.2f} for a threshold of {:.2f}.'.format(f1_score, threshold), fontsize=12)
+    ax1.plot(thresholds, f1_scores, '.-')
+    ax1.plot([threshold, threshold], [0, 1], '-', label='Optimal F1')
+    ax1.legend()
+    ax1.set_xlabel('Threshold')
+    ax1.set_ylabel('F1 Score')
+    ax1.set_xlim([0, 1])
+    ax1.set_ylim([0, 1])
+    
+    # Get PR curve
+    prc_precisions, prc_recalls, prc_thresholds = precision_recall_curve(y.values.flatten(), model.predict_proba(X)[:, 1])
+    precision_opt = precision_score(y.values.flatten(), (model.predict_proba(X)[:, 1] >= threshold).astype(int))
+    recall_opt = recall_score(y.values.flatten(), (model.predict_proba(X)[:, 1] >= threshold).astype(int))
+    
+    # Plot
+    ax2.set_title('Optimal Precision is {:.2f} and recall is {:.2f}.'.format(precision_opt, recall_opt), fontsize=12)
+    ax2.plot(prc_recalls, prc_precisions)
+    ax2.plot(recall_opt, precision_opt, 'o', label='Optimal PR')
+    ax2.legend()
+    ax2.set_xlabel('Recall')
+    ax2.set_ylabel('Precision')
+    ax2.set_xlim([0, 1])
+    ax2.set_ylim([0, 1])
+    
+
+def plot_roc(y_true, y_prob):
+    """
+    Plots the roc curve for given
+    y_true {0,1}, y_predicted probabilities, both 1d array like.
+    y_prob should = model.predict_proba(X_train)
+    """
+    # Get the false pos rate, true pos rate, and thresholds
+    fpr, tpr, thresholds = roc_curve(y_true, y_prob[:,1], pos_label=1)
+    
+    # plot false positive rate vs true positive rate and compare to the 1:1 line (which is random guess)
+    fig, ax = plt.subplots(figsize=(12,8))
+    plt.plot(fpr, tpr, label='model')
+    fig.suptitle('ROC Curve for Model')
+    plt.xlabel('False Positivity Rate (1- Specificity)')
+    plt.ylabel('Ture Positivity Rate (1- Specificity)') 
+    plt.plot([0,1],[0,1], transform=ax.transAxes, label='random guess')
+    plt.legend()
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
